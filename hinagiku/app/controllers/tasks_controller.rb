98f66d363350e888acb83e22866d7164c118cdca
masayuki oguni <masayuki.oguni.dev@gmail.com>
2012-12-25 11:40:21 +0900

    add error

--- hinagiku/app/controllers/tasks_controller.rb
+++ hinagiku/app/controllers/tasks_controller.rb
@@ -26,16 +26,23 @@ class TasksController < ApplicationController
     @task = Task.new
   end
   def create
-  	@task = Task.create(params[:task])
-  	redirect_to @task
+  	@task = Task.new(params[:task])
+    if @task.save
+      redirect_to @task
+    else
+      render :new
+    end
   end
   def edit
     @task = Task.find(params[:id])
   end
-  def update 
-  	@task = Task.find(:params[:id])
-  	@task.update_attributes(params[:task])
-  	redirect_to @task
+  def update
+    @task = Task.find(params[:id])
+    if @task.update_attributes(params[:task])
+      redirect_to @task
+    else
+      render :edit
+    end
   end
   def destroy
   	@task = Task.find(params[:id])
